2dba6426e6aadcd35fb446ca2016b53d477e124d
unknown <mehedi15-2995@diu.edu.bd>
2022-08-25 10:55:16 +0600

    spread added

---
+++ js/spread.js
@@ -0,0 +1,9 @@
+const max = Math.max(12,85,999,78);
+// console.log(max)
+const numbers = [12,85,999,78];
+const largest = Math.max(...numbers);
+// console.log(largest);
+
+const number2 = numbers;
+console.log(number2)
+console.log(numbers);
